eb14066aecd7a1cac0754e3b07292caf5265ef6f
Conor Barthel <conor.barthel@gmail.com>
2022-05-18 15:54:50 -0600

    updated delete action

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -1,5 +1,5 @@
 class Disc < ApplicationRecord
-  has_many :disc_warehouses
+  has_many :disc_warehouses, dependent: :destroy 
   has_many :warehouses, through: :disc_warehouses
   validates :name, presence: true
   validates :plastic, presence: true
